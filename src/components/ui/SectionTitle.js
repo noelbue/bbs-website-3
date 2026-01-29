5464434cd3701c2c7f1a27380bd0b0d3a42bdc7c
Noel Bürgler <mail@noelbuergler.ch>
2026-01-29 10:46:27 +0100

    test

---
+++ src/components/ui/SectionTitle.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as styles from "./SectionTitle.module.css";
+
+const SectionTitle = ({ children, className = "" }) => {
+  return <h2 className={`${styles.sectionTitle} ${className}`}>{children}</h2>;
+};
+
+export default SectionTitle;

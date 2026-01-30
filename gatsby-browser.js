c2f6a885a3fe305e10ef5e7de8de54bba97f5011
Noel Bürgler <mail@noelbuergler.ch>
2026-01-30 16:06:51 +0100

    bugfix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
 import "./src/styles/global.css";
+export const onRouteUpdate = ({ location }) => {
+  if (!document.title || document.title.includes("b-business-solutions.ch")) {
+    document.title = "Bürgler Business Solutions";
+  }
+};
